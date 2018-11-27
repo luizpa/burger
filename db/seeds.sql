e3f3815ebcd41a0f646276e522d412b6e4f40940
Coding <coding@Paulo.local>
2018-11-27 18:37:56 -0500

    gotta fix some stuff

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hemi', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hangry Boy', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Patty', FALSE);
+
+
